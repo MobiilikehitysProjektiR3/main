d76d59019bee58c41ef1ff5e1d8bc82e2c185fb5
Samulikoulu <t1casa00@students.oamk.fi>
2023-03-22 21:32:32 +0200

    V0.01

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.quizzapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById(R.id.loginButton) as Button
+        loginButton.setOnClickListener {
+
+        }
     }
 }
